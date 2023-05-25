848634c5851064a76e63b0e2a2193e5d3f8180f5
appaandbuki <104958634+rachelCilla@users.noreply.github.com>
2023-05-25 13:19:49 -0600

    adds PosesByBenefit

--- client/src/components/poses/PosesByBenefit.js
+++ client/src/components/poses/PosesByBenefit.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import posesBenefitData from './PoseData';
 
+
+
+// index = index of poseBD objs= [{},{},{},{}]
+// object = object of poseBD = [{},{},{},{}]
+// mainCategories = "Symptoms", "Stretches", etc
+// subCategories= the keys of the object that is the value of mainCategories. "Strengthens the abdomen", "Stimulates lungs"
 export default function PosesByBenefit() {
   const [activeItem, setActiveItem] = useState(null);
 
@@ -8,63 +14,42 @@ export default function PosesByBenefit() {
     if (activeItem === index) {
       setActiveItem(null);
     } else {
-      setActiveItem(index);
+      setActiveItem(index); //set the clicked item as active
     }
   };
+  
+ const openPoses = ()=>{
+	
+ }
 
   return (
     <div className="accordion">
         <h1>Find Yoga Poses by Benefit</h1>
 
-        {/* HEADING OF ACCORDION */}
-      {posesBenefitData.map((benefit,index) => (
-        <div className="accordion-item" key={index}>
-          <div
-            className={`accordion-header ${activeItem === index ? 'active' : ''}`}
-            onClick={() => toggleAccordion(index)}
-          >
-            {Object.keys(benefit)}
-          </div>
+        {posesBenefitData.map((object,index) => (
+              <div className="accordion-item" key={index}>
+                  {/* HEADING OF ACCORDION */}
+                    <div className={`accordion-header ${activeItem === index ? 'active' : ''}`}
+                      onClick={() => toggleAccordion(index)}>
+                      {/* accordion button t */}
+                      {Object.keys(object)}
+                    </div>
 
-          {/* BODY OF ACCORDION */}
-
-          {/* when you click a button, benefit of that button (objectkey of posesBD) becomes activeItem (state) */}
-          {/* if activeItem === benefit, render.... */}
-          {activeItem === index && (
-            <div className="accordion-content">
-                <div>
-
-                    {
-
-
-
-
-                 
-                    const categoryKeys = Object.keys(posesBenefitData[index]);
-
-                    // console.log(categoryKeys)
-                   
-
-                    categoryKeys.forEach((key) => {
-                        const benefitObj = category[key];
-                        // console.log(benefitObj)
-                        const benefitKeys = Object.keys(benefitObj);
-                        console.log(benefitKeys)
+                  {/* BODY OF ACCORDION */}
+                  {/* when you click a button, benefit of that button (objectkey of posesBD) becomes activeItem (state) */}
+                  {/* if activeItem === benefit, render.... */}
+                  {activeItem === index && (
+                    <div className="accordion-content">
+						{/* object = {Strengthens: {…}} or {Stimulates: {…}}, */}
                     
-                   }
-                     )
-
-
-
-
-                }
-                </div>
-
-
-            </div>
-          )}
-        </div>
-      ))}
+                        {Object.keys(object[Object.keys(object)]).map((subCategory) => (
+                          <button key={subCategory} onClick={openPoses}>{subCategory}</button>
+                        ))}
+					
+                    </div>
+                  )}
+              </div>
+        ))}
     </div>
   );
 }
